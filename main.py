c744fd972bcfd396714c0c74a3f17ab7f0240ee9
Artur <ArthurZ43@yandex.ru>
2023-01-04 16:31:22 +0300

    added docs

--- main.py
+++ main.py
@@ -21,29 +21,23 @@ if __name__ == '__main__':
 
     sql = SqlRequests(session=session)
 
-    # publisher = sql.add_publisher(table_name="Publisher", name="Gogol")
-    # book = sql.add_book(table_name="Book", title="Book 1", id_publisher=publisher)
-    # shop = sql.add_shop(table_name="shop", name="Буквоед")
-    # sql.add_book_to_shop(table_name="stock", title="Book 1", publisher="Gogol", shop="Буквоед", add_count=10)
-    # sql.add_sale(table_name="Sale", title="Book 1", publisher="Gogol", shop="Буквоед", sold_count=3, price=250)
-
     sql.get_sales(publisher="Pearson")
 
+    # Task 3
+    request_commands = {
+        'publisher': sql.add_publisher,
+        'book': sql.add_book,
+        'shop': sql.add_shop,
+        'stock': sql.add_book_to_shop,
+        'sale': sql.add_sale
+    }
 
-    # request_commands = {
-    #     'publisher': sql.add_publisher,
-    #     'book': sql.add_book,
-    #     'shop': sql.add_shop,
-    #     'stock': sql.add_book_to_shop,
-    #     'sale': sql.add_sale
-    # }
-
-    # with open("test_data.json", encoding='utf-8', mode='r') as file:
-    #     data = json.load(file)
-    #     for row in data:
-    #         print(row)
-    #         cmd = row['model']
-    #         request_commands[cmd](table_name=cmd, kwargs=row['fields'])
+    with open("test_data.json", encoding='utf-8', mode='r') as file:
+        data = json.load(file)
+        for row in data:
+            print(row)
+            cmd = row['model']
+            request_commands[cmd](table_name=cmd, kwargs=row['fields'])
 
 
     session.close()
--- sql_requests.py
+++ sql_requests.py
@@ -2,14 +2,19 @@ import datetime
 from models import Publisher, Book, Shop, Stock, Sale
 
 
-class SqlRequests():
+class SqlRequests:
 
     def __init__(self, session):
 
         self.session = session
         self.tables = {"Publisher": Publisher, "Book": Book, "Shop": Shop, "Stock": Stock, "Sale": Sale}
 
-    def add_publisher(self, table_name: str, kwargs):
+    def add_publisher(self, table_name: str, kwargs: dict):
+        """
+
+        :param table_name:
+        :param kwargs: {'name': ...}
+        """
 
         session = self.session
         table = self.tables[table_name.capitalize()]
@@ -24,7 +29,13 @@ class SqlRequests():
             print(f'{new_data} uploaded to {table_name}')
             return new_data.id
 
-    def add_book(self, table_name: str, kwargs):
+    def add_book(self, table_name: str, kwargs: dict):
+        """
+
+        :param table_name:
+        :param kwargs: {'title': ..., 'id_publisher': ...}
+        :return:
+        """
 
         session = self.session
         table = self.tables[table_name.capitalize()]
@@ -36,13 +47,25 @@ class SqlRequests():
             session.commit()
             print(f'{new_data} uploaded to {table_name}')
 
-    def add_shop(self, table_name: str, kwargs):
+    def add_shop(self, table_name: str, kwargs: dict):
+        """
+
+        :param table_name:
+        :param kwargs: {'name': ...}
+        :return:
+        """
 
         res = self.add_publisher(table_name, kwargs)
 
         return res
 
-    def add_book_to_shop(self, table_name: str, kwargs):
+    def add_book_to_shop(self, table_name: str, kwargs: dict):
+        """
+
+        :param table_name:
+        :param kwargs: {'id_book': ..., 'id_shop': ..., 'count': ...}
+        :return:
+        """
 
         session = self.session
         table = self.tables[table_name.capitalize()]
@@ -57,9 +80,15 @@ class SqlRequests():
             session.query(table).filter(table.id_book == id_book, table.id_shop == id_shop)\
                 .update({'count': count + kwargs['count']})
         session.commit()
-        print(f'{kwargs["count"]} added. Total count: {count + kwargs["count"]}')
+        print(f'{kwargs["count"]} of {id_book} added to shop {id_shop}. Total count: {count + kwargs["count"]}')
 
-    def add_sale(self, table_name: str, kwargs):
+    def add_sale(self, table_name: str, kwargs: dict):
+        """
+
+        :param table_name:
+        :param kwargs: {'price': ..., 'count': ..., 'date_sale': ..., 'id_stock': ...}
+        :return:
+        """
 
         session = self.session
         table = self.tables[table_name.capitalize()]
@@ -74,7 +103,7 @@ class SqlRequests():
                 session.query(Stock).filter(Stock.id == id_stock[0].id)\
                     .update({'count': id_stock[0].count - kwargs['count']})
                 session.commit()
-                print(f'{kwargs["count"]} from "{id_stock}" was sold {date_sale}')
+                print(f'{kwargs["count"]} from stock "{id_stock[0].id}" was sold {date_sale}')
             else:
                 print(f'Not enough count. Available count = {id_stock[0].count}')
         else:
